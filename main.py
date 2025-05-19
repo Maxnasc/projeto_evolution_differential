32473420336c88ad09c5f94d0c365e80dbf1a358
Maxnasc7 <maxnasc7@outlook.com>
2025-05-19 09:06:51 -0300

    commit to save

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plt
 import itertools
 import statistics
 
-from de_algorithm import run_differential_evolution
+from clonalg_algorithm import run_clonal_g
 
 
-def get_result_and_plot(generations, m, data_config, F, CR, opposition, config_code):
+def get_result_and_plot(generations, m, data_config, SR, CR, gamma, opposition, config_code):
     all_results = []
     all_scores_g = []
     best_result = None
     best_score = float('inf')  # ou float('-inf') dependendo se você minimiza ou maximiza
 
-    for i in range(30):
-        result, scores_g = run_differential_evolution(generations, m, data_config, F, CR, opposition)
+    for i in range(1):
+        result, scores_g = run_clonal_g(generations=generations, P=m, SR=SR, CR=CR, gama_seed=gamma, data_config=data_config, opposition=opposition)
         all_results.append(result)
         all_scores_g.append(scores_g)
 
@@ -34,18 +34,18 @@ def get_result_and_plot(generations, m, data_config, F, CR, opposition, config_c
     return {
         "best_result": best_result,
         "mean_scores_g": mean_scores_g,
-        "F": F,
+        "SR": SR,
         "CR": CR,
+        "gamma": gamma,
         "opposition": opposition,
         "generations": generations,
         "m": m,
         "problem": data_config.get("problem")
     }
 
-
-if __name__ == "__main__":
-    generations_list = [30]
-    m_list = [100]
+def your_function_to_track():
+    generations = 30
+    m = 100
     data_config = {
         "problem": "shubert",
         "range": {
@@ -59,17 +59,18 @@ if __name__ == "__main__":
     }
     all_results = []
 
-    Fs = [0.5, 1.0, 2.0]
-    CRs = [0.9, 0.7, 0.5]
+    SR = 0.8
+    CRs = [0.45, 0.6, 0.8]
     oppositions = [True, False]
+    gamma = 0.1  # Constante
 
     config_counter = 1
 
     for problem, target in {"shubert": -186.7309, "camel": -1.0316}.items():
         data_config["problem"] = problem
-        for generations, m, F, CR, opposition in itertools.product(generations_list, m_list, Fs, CRs, oppositions):
-            print(f"Rodando: prob={problem}, gen={generations}, m={m}, F={F}, CR={CR}, opp={opposition}, config={config_counter}")
-            result_dict = get_result_and_plot(generations, m, data_config, F, CR, opposition, config_counter)
+        for CR, opposition in itertools.product(CRs, oppositions):
+            print(f"Rodando: prob={problem}, gen={generations}, m={m}, SR={SR}, CR={CR}, gamma={gamma}, opp={opposition}, config={config_counter}")
+            result_dict = get_result_and_plot(generations, m, data_config, SR, CR, gamma, opposition, config_counter)
             all_results.append(result_dict)
             config_counter += 1
 
@@ -85,7 +86,7 @@ if __name__ == "__main__":
         plt.grid(True)
         plt.xticks(
             range(1, len(data) + 1),
-            [f"F={r['F']}, CR={r['CR']}, Opp={r['opposition']}" for _, r in data.iterrows()],
+            [f"CR={r['CR']}, Opp={r['opposition']}" for _, r in data.iterrows()],
             rotation=45,
             ha="right"
         )
@@ -96,9 +97,11 @@ if __name__ == "__main__":
     best_shubert = df[df["problem"] == "shubert"].sort_values(by="best_result").iloc[0]
     print('Melhor Shubert:')
     print(best_shubert.drop(columns=['mean_scores']))
+    best_shubert.to_csv('melhores_fitness_shubert_clonalg.csv')
     best_camel = df[df["problem"] == "camel"].sort_values(by="best_result").iloc[0]
     print('Melhor Camel:')
     print(best_camel.drop(columns=['mean_scores']))
+    best_camel.to_csv('melhores_fitness_camel_clonalg.csv')
 
     # === Leitura dos dados do GA
     try:
@@ -106,13 +109,16 @@ if __name__ == "__main__":
         df_ga_camel_GA = pd.read_csv("previous_results/melhores_fitness_camel_GA.csv", header=None)
         df_ga_shubert_PSO = pd.read_csv("previous_results/melhores_fitness_shubert_PSO.csv", header=None)
         df_ga_camel_PSO = pd.read_csv("previous_results/melhores_fitness_camel_PSO.csv", header=None)
+        df_ga_shubert_de = pd.read_csv("previous_results/melhores_fitness_shubert_de.csv", header=None)
+        df_ga_camel_de = pd.read_csv("previous_results/melhores_fitness_camel_de.csv", header=None)
 
         # === Gráfico de comparação SHUBERT
         plt.figure(figsize=(8, 5))
         plt.axhline(y=-186.7309, color="r", linestyle="--", label="Valor esperado (target)")
-        plt.plot(range(1, len(best_shubert["mean_scores_g"]) + 1), best_shubert["mean_scores_g"], 'o-', label=f"DE (F={best_shubert['F']}, CR={best_shubert['CR']}, Opp={best_shubert['opposition']})", color="blue")
+        plt.plot(range(1, len(best_shubert["mean_scores_g"]) + 1), best_shubert["mean_scores_g"], 'o-', label=f"CLONALG (CR={best_shubert['CR']}, Opp={best_shubert['opposition']})", color="blue")
         plt.plot(ast.literal_eval(df_ga_shubert_GA.iloc[7].values[1]), 'x-', label="GA", color="green")
         plt.plot(ast.literal_eval(df_ga_shubert_PSO.iloc[4].values[1]), 'x-', label="PSO", color="orange")
+        plt.plot(ast.literal_eval(df_ga_shubert_de.iloc[1].values[1]), 'x-', label="DE", color="red")
         plt.title("Comparação da evolução do GA e DE - Shubert")
         plt.xlabel("Gerações")
         plt.ylabel("Score")
@@ -123,9 +129,10 @@ if __name__ == "__main__":
         # === Gráfico de comparação CAMEL
         plt.figure(figsize=(8, 5))
         plt.axhline(y=-1.0316, color="r", linestyle="--", label="Valor esperado (target)")
-        plt.plot(range(1, len(best_camel["mean_scores_g"]) + 1), best_camel["mean_scores_g"], 'o-', label=f"DE (F={best_camel['F']}, CR={best_camel['CR']}, Opp={best_camel['opposition']})", color="blue")
+        plt.plot(range(1, len(best_camel["mean_scores_g"]) + 1), best_camel["mean_scores_g"], 'o-', label=f"clonalf (CR={best_camel['CR']}, Opp={best_camel['opposition']})", color="blue")
         plt.plot(ast.literal_eval(df_ga_camel_GA.iloc[7].values[1]), 'x-', label="GA", color="green")
         plt.plot(ast.literal_eval(df_ga_camel_PSO.iloc[4].values[1]), 'x-', label="PSO", color="orange")
+        plt.plot(ast.literal_eval(df_ga_camel_de.iloc[1].values[1]), 'x-', label="DE", color="red")
         plt.title("Comparação da evolução do GA e DE - Camel")
         plt.xlabel("Gerações")
         plt.ylabel("Score")
@@ -136,3 +143,7 @@ if __name__ == "__main__":
         print("Arquivos 'melhores_fitness_shubert.csv' ou 'melhores_fitness_camel.csv' não encontrados para comparação com GA.")
 
     plt.show()
+
+if __name__ == "__main__":
+    your_function_to_track()
+    
